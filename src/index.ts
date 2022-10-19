9a2340476445ba81f4a48fc59b17abc5e36641f4
Maurisz <p2002adam@gmail.com>
2022-10-19 14:21:21 +0200

    Interface and classes done

--- src/index.ts
+++ src/index.ts
@@ -36,3 +36,65 @@ class Football implements Results{
     }  
 
 }
+
+
+class Marathon implements Results{
+
+    winner: string;
+
+    date: Date;
+
+    constructor(winner : string, date : Date){
+
+        this.winner = winner;
+        this.date = date;
+
+    }
+
+    result(): string {
+        return "Marathon: " + this.date.getMinutes()+ " min " + this.date.getSeconds() + " s";
+    }
+}
+
+
+class CalvinBall implements Results{
+
+    #winner: string;
+
+    #score : number;
+
+    constructor(winner : string){
+        this.#winner = winner;
+        this.#score = Math.floor(Math.random() * (100 - 10) ) + 10;
+    }
+
+    get winner(){
+        return this.#winner;
+    }
+
+    set winner(winner : string){
+
+        if(winner == "Calvin" || winner == "Hobbes"){
+
+            this.#winner = winner;
+
+        }else{
+            console.log("Hiba történt");
+        }
+    }
+
+    get date() {
+        return this.date;
+    }
+
+    set date(date : Date){
+        this.date = date;
+    }
+
+    result(): string {
+        return "CalvinBall: " + this.#score + " points"
+    }
+
+    
+
+}
